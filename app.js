4bd59ad57f9cfd2b44250e76938f29bf067b6072
0thw0foxa0 <97839499+0thw0foxa0@users.noreply.github.com>
2023-02-28 23:47:03 +0700

    fix

--- app.js
+++ app.js
@@ -1,126 +1,167 @@
 const cash=document.getElementById('cash')
 const cashPS=document.getElementById('cashPS')
 const upp1=document.getElementById('upp1')
-let a=b=c=d=e=f=0;
-
-function notEnoughtMoney(){
-    console.log('not enought money for uppgrade');
-}
-
-function fullUpp(){
-    console.log('you have bought all the upgrades of this type')
-}
-
-function clickplus(){
-       document.getElementById('cash').innerHTML=Number((i++).toFixed(1));
-}
-
-function plusUpp1(){
-    if(a>100)
-    {
-        fullUpp()
-    }
-   else if(i<10 )
-    {
-        notEnoughtMoney();
-    }
-    else if(i>=10 && a<100)
-    { i-=10;
-        a++;
-      document.getElementById('upp1').innerHTML="+ 1/сек <br> У вас "+a;
-      firstValue++;
-    }
-}
-
-
-function plusUpp2(){
-    if(b>50)
-    {
-        fullUpp()
-    }
-    else if(i<100 )
-    {
-        notEnoughtMoney();
-    }
-    else if(i>=100 && b<50)
-    { i-=100;
-        b++;
-      document.getElementById('upp2').innerHTML="+ 15/сек <br> У вас "+b;
-      firstValue+=15;
-    }
-}
-
-function plusUpp3(){
-    if(c>30)
-    {
-        fullUpp()
-    }
-    else if(i<1000 )
-    {
-        notEnoughtMoney();
-    }
-    else if(i>=1000 && c<30)
-    { i-=1000;
-        c++;
-      document.getElementById('upp3').innerHTML="+ 55/сек <br> У вас "+c;
-      firstValue+=55;
-    }
-}
-
-function plusUpp4(){
-    if(d>25)
-    {
-        fullUpp()
-    }
-    else if(i<10000 )
-    {
-        notEnoughtMoney();
-    }
-    else if(i>=10000 && d<25)
-    { i-=10000;
-        d++;
-      document.getElementById('upp4').innerHTML="+ 220/сек <br> У вас "+d;
-      firstValue+=220;
-    }
-}
-
-function plusUpp5(){
-    if(e>15)
-    {
-        fullUpp()
-    }
-   else if(i<100000 )
-    {
-        notEnoughtMoney();
-    }
-    else if(i>=100000 && e<15)
-    { i-=100000;
-        e++;
-      document.getElementById('upp5').innerHTML="+ 1240/сек <br> У вас "+e;
-      firstValue+=1240;
-    }
-}
-
-function plusUpp6(){
-    if(f>10)
-    {
-        fullUpp()
-    }
-    else if(i<1000000 )
-    { 
-        notEnoughtMoney();
-    }
-    else if(i>=1000000 && f<10)
-    { i-=1000000;
-        f++;
-      document.getElementById('upp6').innerHTML="+ 1240/сек <br> У вас "+f;
-      firstValue+=9700;
-    }
-}
 
 let firstValue=0;
 let i = 1;
 let interval = setInterval( increment, 100);
+let arrCost=[
+    10,
+    100,
+    1000,
+    10000,
+    100000,
+    1000000,
+];
+
+let arrCashPS=[
+    1,
+    15,
+    55,
+    220,
+    1240,
+    9700,
+];
+
+let arrCashPSsum=
+[
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+
+]
+let arrCount=[0,0,0,0,0,0];
+// подгрузка и выгрузка 
+if(localStorage.getItem('i')!= null){
+    i = +localStorage.getItem('i');
+    firstValue = +localStorage.getItem('firstValue');
+let tmp=0;
+for(m=0; m<6; m++)
+{ 
+   arrCashPS[m]=+localStorage.getItem(`arrCashPS${m}`)
+}
+
+for(m=0; m<6; m++)
+{ 
+   arrCashPSsum[m]=+localStorage.getItem(`arrCashPSsum${m}`)
+}
+
+for(m=0; m<6; m++)
+{ 
+   arrCount[m]=+localStorage.getItem(`arrCount${m}`)  
+}
+
+for(m=0; m<6; m++)
+{ 
+   arrCost[m]=+localStorage.getItem(`arrCost${m}`)
+   
+}
+
+
+
+    }
+
+
+    window.onbeforeunload = function() {                
+	localStorage.setItem('i', i);
+    localStorage.setItem('firstValue', firstValue);
+    for(m=0; m<6; m++)
+    { 
+    tmp= arrCashPS[m];
+    localStorage.setItem(`arrCashPS${m}`,tmp);
+    }
+
+    for(m=0; m<6; m++)
+    { 
+    tmp= arrCashPSsum[m];
+    localStorage.setItem(`arrCashPSsum${m}`,tmp);
+    }
+    for(m=0; m<6; m++)
+    { 
+       tmp= arrCount[m];
+       localStorage.setItem(`arrCount${m}`,tmp);
+       
+    }
+    
+    for(m=0; m<6; m++)
+    { 
+    tmp= arrCost[m];
+    localStorage.setItem(`arrCost${m}`,tmp);
+   
+    }
+};
+//
+
+
+function digit(numUpp)
+{
+    if(arrCost[numUpp]>=1000 && arrCost[numUpp]<1000000)
+    {
+        document.getElementById('costUpp' +numUpp).innerHTML="Цена: "+Number((arrCost[numUpp]/1000).toFixed(1))+" K";
+    }
+    else if(arrCost[numUpp]<1000000){
+       document.getElementById('costUpp' +numUpp).innerHTML="Цена: "+Number((arrCost[numUpp]).toFixed(1));
+    }   
+    else if( arrCost[numUpp]>=1000000)
+    {
+        document.getElementById('costUpp' +numUpp).innerHTML="Цена: "+Number((arrCost[numUpp]/1000000).toFixed(1))+" M";   
+    }
+    
+}
+
+
+
+
+function notEnoughtMoney()
+{
+    console.log('not enought money for uppgrade');
+}
+
+function fullUpp()
+{
+    console.log('you have bought all the upgrades of this type')
+}
+
+function clickplus()
+{
+    document.getElementById('cash').innerHTML=Number((i++).toFixed(1));
+}
+
+function plusUpp(numUpp)
+{
+    if(numUpp==0 && arrCount[numUpp]==35)
+    {
+     fullUpp();
+    }
+
+    else if(arrCount[numUpp]==50)
+    {
+        fullUpp();
+    }
+
+    else if(i<arrCost[numUpp])
+    {
+        notEnoughtMoney();
+    }
+    else if(i>=arrCost[numUpp])
+{ 
+    console.log('- '+ arrCost[numUpp]);
+        i-=arrCost[numUpp];
+        firstValue+=arrCashPS[numUpp];
+        arrCost[numUpp]+=arrCost[numUpp]/5;
+       arrCount[numUpp]++;
+    console.log('costUpp'+numUpp)
+    arrCashPSsum[numUpp]+=arrCashPS[numUpp]
+    document.getElementById('upp' +numUpp).innerHTML="+ "+arrCashPSsum[numUpp]+" в сек"  +"<br> у вас "+ arrCount[numUpp]; 
+   digit(numUpp);
+}
+    
+}
+
+
 
 function increment(){
    
@@ -128,9 +169,18 @@ function increment(){
     console.log(i);
     document.getElementById('cash').innerHTML=Number((i).toFixed(1));
     document.getElementById('cashPS').innerHTML="+ "+firstValue+" /сек";
+    for(m=0; m<6; m++){
+        document.getElementById('upp' +m).innerHTML="+ "+arrCashPSsum[m]+" в сек"  +"<br> у вас "+ arrCount[m]; 
+        digit(m);
+    }
+    
+
 if(firstValue==123600)
 {
     alert('Ну и нахуй ты игру прошел, че делать теперь? ну радуйся хули')
     firstValue=999999;
 }
 }
+
+
+
